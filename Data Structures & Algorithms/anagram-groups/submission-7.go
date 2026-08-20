ac32a2c2f08fac1620a12b9ddd3712eda4cb0cb7
Raul Luis <soyrauluis@gmail.com>
2026-08-20 15:24:06 -0400

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.go
@@ -0,0 +1,30 @@
+type FrequencyArray [27]int
+
+func groupAnagrams(strs []string) [][]string {
+ if len(strs) < 1 {
+        return [][]string{}
+    }
+	var lower func(rune) rune 
+	lower = func(c rune) rune {
+		if c >= 'A' && c <= 'Z' {
+			return c + 'a' - 'A'
+		}
+		return c
+	}
+
+    m := map[FrequencyArray][]string{}
+    for _, str := range strs {
+		fm := FrequencyArray{}
+		for _, r := range str {
+			fm[lower(r)-97]++
+		}
+
+        m[fm] = append(m[fm], str)
+    }
+
+	arr := [][]string{}
+   for _, v := range m {
+    arr = append(arr, v)
+   }
+   return arr
+}
