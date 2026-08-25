cb1afe4f1037186a8425cd0ce7ae278e77d5ed7d
Raul Luis <soyrauluis@gmail.com>
2026-08-25 04:27:39 -0400

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.cpp
@@ -0,0 +1,25 @@
+#include <cctype>
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+       int l = 0, r = s.size() - 1;
+        while (l < r) {
+            if(!isalnum(s[r])) {
+                r--;
+                continue;
+            } 
+           if (!isalnum(s[l])){
+                l++;
+                continue;
+            }
+            if (tolower(s[l]) != tolower(s[r])) {
+                return false;
+            }
+            r--;
+            l++;
+        }
+        return true;
+ 
+    }
+};
